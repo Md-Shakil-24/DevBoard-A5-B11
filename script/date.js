737b59cc629e288b80f4c817d510e974ca08e1c4
Md-Shakil-24 <shakilahmed14068682@gmail.com>
2025-03-02 18:56:48 +0600

    First

---
+++ script/date.js
@@ -0,0 +1,9 @@
+function updateDate() {
+    let d = new Date();
+    let options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+    let parts = d.toLocaleDateString('en-US', options).split(" ");
+    document.getElementById("weekday").textContent = parts[0];
+    document.getElementById("date").textContent = parts[1] + " " + parts[2] + " " + parts[3];
+}
+updateDate();
+setInterval(updateDate, 60000);
---
+++ script/dev.js
@@ -0,0 +1,3 @@
+document.getElementById("myButton").onclick = function() {
+    window.location.href = "dev.html"; 
+};
